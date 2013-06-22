2c21f24e8da980748526bd172406cae5a9d6c5bc
Herman Junge <herman@forkhq.com>
2013-06-21 21:04:56 -0400

    wip

--- dist/windows/README.md
+++ dist/windows/README.md
@@ -2,15 +2,14 @@ MSI Package Knowledge Base
 
 ==============================================
 
-For now, as 20130613, we will have the binaries in the folder __dirname/dist/windows/bin.
+For now, as 20130613, we will have the binaries in the folder __dirname/dist/windows/bin. (In the respective branch win-msi).
 
 In the future, let's only have the links to these files mentioned.
 
 Currently you need:
 
 * Wix
-* Paraffine
-
+* Paraffine (for fragment creation, there are +1000 files to be indexed)
 
 # Files involved
 
@@ -21,3 +20,15 @@ Currently you need:
 ## main.wxs
 
 (TODO)
+
+# Problemas a resolver
+
+
+- Tenemos que resolver el problema de la version (poner 0.10.0 y que quede bien)
+- Tenemos que resolver el problema de llamar a:
+	 - config activate
+	 - post install
+	 - config gui
+- despues arreglar la gui
+- despues instalar .NET si no lo tiene
+- agregar un icono a la instalación de prey
--- dist/windows/make_installer.cmd
+++ dist/windows/make_installer.cmd
@@ -1,51 +1,15 @@
 
 REM "Creates an MSI package for prey"
 
-set version=0.10.0
-
 del prey-msi.*
 del main.wixobj
 
-rd /s /q versions
-mkdir versions
-xcopy ..\..\bin versions\%version%\bin /i /s
-xcopy ..\..\lib versions\%version%\lib /i /s
-
-mkdir versions\%version%\node_modules
-xcopy ..\..\node_modules\commander versions\%version%\node_modules\commander /i /s
-xcopy ..\..\node_modules\getset versions\%version%\node_modules\getset /i /s
-xcopy ..\..\node_modules\qs versions\%version%\node_modules\qs /i /s
-xcopy ..\..\node_modules\needle versions\%version%\node_modules\needle /i /s
-xcopy ..\..\node_modules\xml2js versions\%version%\node_modules\xml2js /i /s
-xcopy ..\..\node_modules\dialog versions\%version%\node_modules\dialog /i /s
-xcopy ..\..\node_modules\mime versions\%version%\node_modules\mime /i /s
-xcopy ..\..\node_modules\reply versions\%version%\node_modules\reply /i /s
-xcopy ..\..\node_modules\async versions\%version%\node_modules\async /i /s
-xcopy ..\..\node_modules\underscore versions\%version%\node_modules\underscore /i /s
-xcopy ..\..\node_modules\unzip versions\%version%\node_modules\unzip /i /s
-xcopy ..\..\node_modules\campfire versions\%version%\node_modules\campfire /i /s
-xcopy ..\..\node_modules\nodemailer versions\%version%\node_modules\nodemailer /i /s
-xcopy ..\..\node_modules\connect versions\%version%\node_modules\connect /i /s
-xcopy ..\..\node_modules\entry versions\%version%\node_modules\entry /i /s
-xcopy ..\..\node_modules\triggers versions\%version%\node_modules\triggers /i /s
-
-xcopy ..\..\scripts versions\%version%\scripts /i /s
-xcopy ..\..\test versions\%version%\test /i /s
-
-copy ..\..\index.js versions\%version%\.
-copy ..\..\license.txt versions\%version%
-copy ..\..\package.json versions\%version%
-copy ..\..\prey.conf.defaults versions\%version%
-copy ..\..\README.md versions\%version%
-
 ".\bin\paraffin" prey-msi.wxs ^
--dir versions ^
+-dir source-package ^
 -groupname prey-msi
 
 ".\bin\candle" ^
-prey-msi.wxs main.wxs ^
--ext WixUIExtension -ext WixUtilExtension
+prey-msi.wxs main.wxs
 
 ".\bin\light" ^
--out prey-msi.msi prey-msi.wixobj main.wixobj ^
--ext WixUIExtension -ext WixUtilExtension
+-out prey-msi.msi prey-msi.wixobj main.wixobj
